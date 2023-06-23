15517ee0f3ddc2725504be67b86005264049b56b
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-23 20:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const text = document.getElementById("text");
-const delay = document.getElementById("delay");
-const btn = document.getElementById("btn");
-const output = document.getElementById("output");
+// const text = document.getElementById("text");
+// const delay = document.getElementById("delay");
+// const btn = document.getElementById("btn");
+// const output = document.getElementById("output");
 
-//your code here
-async function showMessage() {
-  const message = text.value;
-  const delayVal = delay.value;
-  await new Promise((resolve) => 
-	  setTimeout(resolve, delayVal));
-  output.innerText = message;
-}
+// //your code here
+// async function showMessage() {
+//   const message = text.value;
+//   const delayVal = delay.value;
+//   await new Promise((resolve) => 
+// 	  setTimeout(resolve, delayVal));
+//   output.innerText = message;
+// }
 
-btn.addEventListener("click", showMessage);
+// btn.addEventListener("click", showMessage);
+
+
+ async function showMessage() {
+            const textInput = document.getElementById('text');
+            const delayInput = document.getElementById('delay');
+            const outputDiv = document.getElementById('output');
+
+            const message = textInput.value;
+            const delay = parseInt(delayInput.value);
+
+
+            await delayFunction(delay);
+            outputDiv.innerText = message;
+        }
+
+        function delayFunction(delay) {
+            return new Promise(resolve => {
+                setTimeout(resolve, delay);
+            });
+        }
+
+        const button = document.getElementById('btn');
+        button.addEventListener('click', showMessage);
